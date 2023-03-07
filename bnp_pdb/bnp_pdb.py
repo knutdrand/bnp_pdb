4326409d02142e34f3b2dc754d515cbe49893fd7
Knut Rand <knutdrand@gmail.com>
2023-03-07 14:00:27 +0100

    Testing parsing

--- bnp_pdb/bnp_pdb.py
+++ bnp_pdb/bnp_pdb.py
@@ -1,1 +1,39 @@
 """Main module."""
+from bionumpy.bnpdataclass import bnpdataclass
+import bionumpy as bnp
+import dataclasses
+
+@bnpdataclass
+class Atom:
+    atom_number: int
+    atom: str
+    residue: str
+    chain: str
+    residue_number: int
+    x: float
+    y: float
+    z: float
+    occupancy: float
+    temperature_factor: float
+    element_name: str
+
+
+def parse_file(file_obj):
+    atom_lines = (line.strip() for line in file_obj if line.startswith('ATOM'))
+    types = [field.type for field in dataclasses.fields(Atom)]
+    args = zip(*((t(p) for t, p in zip(types, line.split()[1:12])) for line in atom_lines))
+    return Atom(*[list(a) for a in args])
+
+
+class PDB:
+
+    def __init__(self, atoms: Atom):
+        self._chains = dict(bnp.groupby(atoms, 'chain'))
+
+    @property
+    def chains(self) -> dict[str, Atom]:
+        return self._chains
+
+    @classmethod
+    def from_atoms(cls, atoms: Atom):
+        return cls(atoms)
